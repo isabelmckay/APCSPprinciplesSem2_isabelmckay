e1418b3b44a5c27733bdafd968292ed69790f14b
isabelmckay <43447413+isabelmckay@users.noreply.github.com>
2019-01-15 08:46:33 -0800

    done with measurements

--- Lab 111 InsertionSort/Insertion.js
+++ Lab 111 InsertionSort/Insertion.js
@@ -5,17 +5,49 @@
 */
 
 // Insertion sort function
-//variables
-var temp;
-      for (var  i = 1; i < arr.length; i++)
-{
-            for(var  j = i ; j > 0 ; j--)
-            {
-//  Swap!
-               if(arr[j] < arr[j-1]){
-                  temp = arr[j];
-                  arr[j] = arr[j-1];
-                  arr[j-1] = temp;
-                }
-            }
-       }
+//global variables
+//measurements
+var compares = 0
+var swaps = 0;
+var millisecondEnd = 0;
+var milliseondStart = 0;
+var data = [];
+var dataNumbers = 20000;
+
+//setup function
+function setup(){
+  for(var i = 0; i < dataNumbers; i++){
+    data.push(random(0, 2000));
+  }
+  organize();
+}
+
+//calling draw
+function draw(){
+}
+
+function organize(){
+  var temp;
+  millisecondStart = millis();
+  for (var i = 1; i < data.length; i++){
+    for(var j = i; j > 0; j--){
+      //comparing
+      compares++;
+      if(data[j] < data[j-1]){
+        temp = data[j];
+        data[j] = data[j-1];
+        data[j-1] = temp;
+        //swapping data
+        swaps++;
+      }
+    }
+  }
+  // finished array
+  millisecondEnd = millis();
+  //prints final sorted array
+  console.log(data);
+  console.log(millisecondStart + " start");
+  console.log(millisecondEnd + " end");
+  console.log(swaps + " swaps");
+  console.log(compares + " compares");
+}
